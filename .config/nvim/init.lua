1047d35d178fc43d437ebf01903fa3b7e399e71c
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-02-11 16:25:20 -0300

    feat: configure more LSPs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -601,14 +601,14 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  tsserver = {},
-
-  sumneko_lua = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
+    tsserver = {},
+    elixirls = {},
+    sumneko_lua = {
+        Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+        },
     },
-  },
 }
 
 local null_ls = require('null-ls')
