3fcc7903fc412ca3fcd6d93d9e81bc41b3f81488
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-02-11 20:42:56 -0300

    feat: configure treesitter with new module

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -109,14 +109,13 @@ require('packer').startup(function(use)
 
   use { -- Highlight, edit, and navigate code
       'nvim-treesitter/nvim-treesitter',
+      requires = {
+          { 'nvim-treesitter/nvim-treesitter-textobjects' },
+          { 'nvim-treesitter/playground' },
+      },
       run = function()
         pcall(require('nvim-treesitter.install').update { with_sync = true })
       end,
-  }
-
-  use { -- Additional text objects via treesitter
-      'nvim-treesitter/nvim-treesitter-textobjects',
-      after = 'nvim-treesitter',
   }
 
   -- Git related plugins
@@ -505,6 +504,24 @@ require('nvim-treesitter.configs').setup {
 
     highlight = { enable = true },
     indent = { enable = true, disable = { 'python' } },
+    playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = false, -- Whether the query persists across vim sessions
+        keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
+        },
+    },
     textobjects = {
         select = {
             enable = true,
