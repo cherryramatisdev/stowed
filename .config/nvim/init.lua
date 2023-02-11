9739e5f2af92d019bbd606c1c34a7ab8c02850d9
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-02-11 16:20:24 -0300

    feat: add more bindings to gitsigns

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -419,11 +419,46 @@ require('gitsigns').setup {
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
     },
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ']c', function()
+        if vim.wo.diff then return ']c' end
+        vim.schedule(function() gs.next_hunk() end)
+        return '<Ignore>'
+      end, { expr = true })
+
+      map('n', '[c', function()
+        if vim.wo.diff then return '[c' end
+        vim.schedule(function() gs.prev_hunk() end)
+        return '<Ignore>'
+      end, { expr = true })
+
+      -- Actions
+      map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+      map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+      map('n', '<leader>hS', gs.stage_buffer)
+      map('n', '<leader>hu', gs.undo_stage_hunk)
+      map('n', '<leader>hR', gs.reset_buffer)
+      map('n', '<leader>hp', gs.preview_hunk)
+      map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+      map('n', '<leader>tb', gs.toggle_current_line_blame)
+      map('n', '<leader>hd', gs.diffthis)
+      map('n', '<leader>hD', function() gs.diffthis('~') end)
+      map('n', '<leader>td', gs.toggle_deleted)
+      map('n', '<leader>hc', [[:TermExec cmd='git commit --no-verify'<cr>]])
+
+      -- Text object
+      map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    end
 }
-vim.keymap.set('n', ']g', ':Gitsigns next_hunk<cr>')
-vim.keymap.set('n', '[g', ':Gitsigns prev_hunk<cr>')
-vim.keymap.set('n', '<leader>gs', ':Gitsigns stage_hunk<cr>')
-vim.keymap.set('n', '<leader>gr', ':Gitsigns reset_hunk<cr>')
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
