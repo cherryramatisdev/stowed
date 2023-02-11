d346a2d789601b10584aa5bba90eee2327aa2eb7
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-02-11 16:01:49 -0300

    feat: add plugins to manage my test in smart way

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -58,6 +58,17 @@ require('packer').startup(function(use)
     }
   end }
 
+  -- Run tests
+  use {
+      "nvim-neotest/neotest",
+      requires = {
+          "nvim-lua/plenary.nvim",
+          "nvim-treesitter/nvim-treesitter",
+          "olimorris/neotest-rspec",
+          "haydenmeade/neotest-jest"
+      }
+  }
+
   use { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     requires = {
@@ -215,6 +226,144 @@ require('lualine').setup {
 
 -- Adding mapping for oil.nvim
 vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+
+-- Enable neotest
+require("neotest").setup({
+    quickfix = {
+        open = false,
+    },
+    adapters = {
+        require("neotest-jest"),
+        require("neotest-rspec"),
+    },
+})
+
+vim.keymap.set("n", "<leader>tf", function()
+  require("neotest").run.run(vim.fn.expand("%"))
+end)
+
+vim.keymap.set("n", "<leader>tt", function()
+  require("neotest").run.run()
+end)
+
+vim.keymap.set("n", "<leader>ta", function()
+  require("neotest").run.attach()
+end)
+
+vim.keymap.set("n", "<leader>tS", function()
+  require("neotest").run.stop()
+end)
+
+vim.keymap.set("n", "<leader>ts", function()
+  require("neotest").summary.open()
+end)
+
+vim.keymap.set("n", "<leader>to", function()
+  require("neotest").output.open()
+end)
+
+-- Enable dressing.nvim
+require('dressing').setup({
+    input = {
+        -- Set to false to disable the vim.ui.input implementation
+        enabled = true,
+
+        -- Default prompt string
+        default_prompt = "Input:",
+
+        -- Can be 'left', 'right', or 'center'
+        prompt_align = "left",
+
+        -- When true, <Esc> will close the modal
+        insert_only = true,
+
+        -- When true, input will start in insert mode.
+        start_in_insert = true,
+
+        -- These are passed to nvim_open_win
+        anchor = "SW",
+        border = "rounded",
+        -- 'editor' and 'win' will default to being centered
+        relative = "cursor",
+
+        -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        prefer_width = 40,
+        width = nil,
+        -- min_width and max_width can be a list of mixed types.
+        -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
+        max_width = { 140, 0.9 },
+        min_width = { 20, 0.2 },
+
+        buf_options = {},
+        win_options = {
+            -- Window transparency (0-100)
+            winblend = 10,
+            -- Disable line wrapping
+            wrap = false,
+        },
+
+        -- Set to `false` to disable
+        mappings = {
+            n = {
+                ["<Esc>"] = "Close",
+                ["<CR>"] = "Confirm",
+            },
+            i = {
+                ["<C-c>"] = "Close",
+                ["<CR>"] = "Confirm",
+                ["<Up>"] = "HistoryPrev",
+                ["<Down>"] = "HistoryNext",
+            },
+        },
+    },
+    select = {
+        -- Set to false to disable the vim.ui.select implementation
+        enabled = true,
+
+        -- Priority list of preferred vim.select implementations
+        backend = { "telescope", "builtin" },
+
+        -- Trim trailing `:` from prompt
+        trim_prompt = true,
+
+        -- Options for built-in selector
+        builtin = {
+            -- These are passed to nvim_open_win
+            anchor = "NW",
+            border = "rounded",
+            -- 'editor' and 'win' will default to being centered
+            relative = "editor",
+
+            buf_options = {},
+            win_options = {
+                -- Window transparency (0-100)
+                winblend = 10,
+            },
+
+            -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+            -- the min_ and max_ options can be a list of mixed types.
+            -- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
+            width = nil,
+            max_width = { 140, 0.8 },
+            min_width = { 40, 0.2 },
+            height = nil,
+            max_height = 0.9,
+            min_height = { 10, 0.2 },
+
+            -- Set to `false` to disable
+            mappings = {
+                ["<Esc>"] = "Close",
+                ["<CR>"] = "Confirm",
+            },
+        },
+
+        -- Used to override format_item. See :help dressing-format
+        format_item_override = {},
+
+        -- see :help dressing_get_config
+        get_config = nil,
+    },
+})
 
 -- Enable Comment.nvim
 require('Comment').setup()
