f34ecf73af6a76431563ecbb5783fda9ebe1120d
Cherry Ramatis <cherry.ramatis@icloud.com>
2024-08-12 10:21:48 -0300

    Add tresitter context to nvim

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,6 +1,26 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter-context',
+        opts = {
+          enable = true,
+          max_lines = 2,
+          min_window_height = 0,
+          line_numbers = true,
+          multiline_threshold = 20,
+          trim_scope = 'outer',
+          mode = 'cursor',
+          separator = nil,
+          zindex = 20,
+          on_attach = nil,
+        },
+        config = function(_, opts)
+          require('treesitter-context').setup(opts)
+        end,
+      },
+    },
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
