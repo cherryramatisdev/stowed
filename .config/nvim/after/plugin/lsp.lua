31512c1e0b65db0ccf44471878819d123294d2d2
Cherry Ramatis <cherry.ramatis@icloud.com>
2024-10-05 17:58:07 -0300

    Improve lua support

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -3,6 +3,19 @@ MiniDeps.add {
   depends = { "williamboman/mason.nvim" },
 }
 
+MiniDeps.add {
+  source = "folke/lazydev.nvim",
+  ft = "lua",
+  depends = { "Bilal2453/luvit-meta" },
+}
+
+require("lazydev").setup {
+  library = {
+    -- Load luvit types when the `vim.uv` word is found
+    { path = "luvit-meta/library", words = { "vim%.uv" } },
+  },
+}
+
 require("mason").setup {}
 
 require("lspconfig").ts_ls.setup {}
