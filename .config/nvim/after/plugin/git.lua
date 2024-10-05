74cf32f6d2c8d080671ded28168e8e22f1fe8cf9
Cherry Ramatis <cherry.ramatis@icloud.com>
2024-10-05 17:58:02 -0300

    Add neogit

--- .config/nvim/after/plugin/git.lua
+++ .config/nvim/after/plugin/git.lua
@@ -1,7 +1,14 @@
 require("mini.notify").setup {}
 require("mini.git").setup {}
 
-vim.keymap.set({ "n", "x" }, "<leader>gs", MiniGit.show_at_cursor)
+MiniDeps.add {
+  source = "NeogitOrg/neogit",
+  depends = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
+}
+
+require("neogit").setup {}
+
+vim.keymap.set("n", "<leader>gs", ":Neogit<CR>")
 
 vim.cmd [[
 au FileType git,diff setlocal foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr() foldlevel=0
